2fa86395546058bb477fdbb823c5783177ba11b4
labcabrera <lab.cabrera@gmail.com>
2018-03-05 15:46:40 +0100

    Upgrade Spring Boot version + cleanup

--- docker-create-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run \
-  --interactive \
-	--tty \
-  --name http-network-diagnostic \
-  --hostname http-network-diagnostic \
-  --publish 8080:8080 \
-  labcabrera/http-network-diagnostic:1.0.0
--- docker-create-image.sh
+++
@@ -1,1 +0,0 @@
-docker build -f Dockerfile -t labcabrera/http-network-diagnostic:1.0.0 .
